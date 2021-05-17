72f5c5a4fa574d87832f7b54afe1de0b06b8d161
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-17 22:26:15 +0600

    Changes

--- Form.php
+++ Form.php
@@ -21,7 +21,7 @@ public function addElement(HtmlElement $el)
 public function render(): string
 {
     $content = implode(PHP_EOL,array_map(fn($el)=> $el->render(), $this->element));
-    return sprintf( '<form action="%s" method="%s">%s</form>',$this->action, $this->method);
+    return sprintf( '<form action="%s" method="%s">%s</form>',$this->action, $this->method, $content);
 }
 
 }
---
+++ TextInput.php
@@ -0,0 +1,6 @@
+<?php
+
+class TextInput{
+
+    
+}
