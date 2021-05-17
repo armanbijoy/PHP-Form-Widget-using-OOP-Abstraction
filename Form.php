b26a7bebdf29095c4845dd8952a1101694051f9f
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-17 22:18:54 +0600

    All Added

--- Form.php
+++ Form.php
@@ -2,7 +2,16 @@
 
 class Form extends HtmlElement{
 
+    public string $action;
+    public string $method;
+
    protected array $element = []; 
+
+   public function __construct(string $action='', string $method='get')
+   {
+       $this->action = $action;
+       $this->method = $method;
+   }
 
 public function addElement(HtmlElement $el)
 {
